3358834d6c8499544baa2116d8bbbbfb97937cf4
Douglas_Wesley <douglaswesley0407@gmail.com>
2025-02-04 23:21:45 -0300

    Criação e definição do CRUD

--- App/models.py
+++ App/models.py
@@ -13,6 +13,47 @@ class Tarefas:
             banco_tarefa = sqlite3.connect('App/database/tarefas.db')
             cursor = banco_tarefa.cursor()
             cursor.execute(f'INSERT INTO tarefas VALUE({self.id},{self.titulo},{self.status})')
+            banco_tarefa.commit()
+            cursor.close()
+            banco_tarefa.close()
             
         except sqlite3.Error as error:
             print(f"Ocorreu um erro: {error}")
+        finally:
+            if cursor:
+                cursor.close()
+            if banco_tarefa:
+                banco_tarefa.close()
+    
+    def ler_tarefa(self):
+        try:    
+            banco_tarefa = sqlite3.connect('App/database/tarefas.db')
+            cursor = banco_tarefa.cursor()
+            read = cursor.execute("SELECT * FROM tarefas")         
+            return read
+        except sqlite3.Error as error:
+            print(f"Ocorreu um erro: {error}")
+        finally:
+            if cursor:
+                cursor.close()
+            if banco_tarefa:
+                banco_tarefa.close()
+    
+    def atualizar_tarefa(self,id,status):
+        try:
+            banco_tarefa = sqlite3.connect('App/database/tarefas.db')
+            cursor = banco_tarefa.cursor()
+            
+            
+            cursor.execute("UPDATE tarefas SET")
+        except sqlite3.Error as error:
+            print(f"Ocorreu um erro: {error}")
+        finally:
+            if cursor:
+                cursor.close()
+            if banco_tarefa:
+                banco_tarefa.close()
+                
+    def excluir_tarefas(self,id):
+        pass
+    
