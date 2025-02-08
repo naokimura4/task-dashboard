1bfa13663dcdeb2ab7ef7ecc156e0e76e2304232
Douglas_Wesley <douglaswesley0407@gmail.com>
2025-02-07 21:59:01 -0300

    Correção do CRUD e finalização desta etapa

--- App/models.py
+++ App/models.py
@@ -10,10 +10,14 @@ class Tarefas:
     def __str__(self):
         return f'{self.titulo} = [{self.status}]'
     
-    def criar_tarefa(self,titulo):
-        try:
+    def conectar(self):
             banco_tarefa = sqlite3.connect('App/database/tarefas.db')
             cursor = banco_tarefa.cursor()
+            return banco_tarefa, cursor
+        
+    def criar_tarefa(self,titulo):
+        try:
+            banco_tarefa, cursor = self.conectar()
             status = 0
             cursor.execute('INSERT INTO tarefas (titulo, status) VALUES(?, ?)',(titulo, status))
             banco_tarefa.commit()
@@ -28,10 +32,12 @@ class Tarefas:
     
     def ler_tarefa(self):
         try:    
-            banco_tarefa = sqlite3.connect('App/database/tarefas.db')
-            cursor = banco_tarefa.cursor()
-            read = cursor.execute("SELECT * FROM tarefas")         
-            return read
+            banco_tarefa, cursor = self.conectar()
+            read = cursor.execute("SELECT * FROM tarefas").fetchall()
+            if read == []:
+                return []
+            else: 
+                return read
         except sqlite3.Error as error:
             print(f"Ocorreu um erro: {error}")
         finally:
@@ -39,15 +45,12 @@ class Tarefas:
                 cursor.close()
             if banco_tarefa:
                 banco_tarefa.close()
-    
+        
     def atualizar_tarefa(self,id,status):
         try:
-            banco_tarefa = sqlite3.connect('App/database/tarefas.db')
-            cursor = banco_tarefa.cursor()            
-            cursor.execute(f"UPDATE tarefas SET status = {self.status} WHERE = {self.id}") # USAR O ? PARA PLACEHOLDER 
+            banco_tarefa, cursor = self.conectar()
+            cursor.execute("UPDATE tarefas SET status = ? WHERE id = ?",(status,id)) # USAR O ? NO LUGAR DO F-STRING 
             banco_tarefa.commit()
-            cursor.close()
-            banco_tarefa.close()
         except sqlite3.Error as error:
             print(f"Ocorreu um erro: {error}")
         finally:
@@ -58,12 +61,9 @@ class Tarefas:
                 
     def excluir_tarefas(self,id):
         try:
-            banco_tarefa = sqlite3.connect('App/database/tarefas.db')
-            cursor = banco_tarefa.cursor()            
-            cursor.execute(f"DELETE FROM tarefas WHERE id = {id}")
+            banco_tarefa, cursor = self.conectar()
+            cursor.execute("DELETE FROM tarefas WHERE id = ?",(id,))
             banco_tarefa.commit()
-            cursor.close()
-            banco_tarefa.close()
         except sqlite3.Error as error:
             print(f"Ocorreu um erro: {error}")
         finally:
@@ -71,4 +71,3 @@ class Tarefas:
                 cursor.close()
             if banco_tarefa:
                 banco_tarefa.close()
-    
